84925a098aad8c45ed9f7c1a1ce7a938efa25182
Ravishankar <Ravishankar@Akella>
2018-06-17 22:39:40 -0500

    First commit

---
+++ hello_world/src/main/java/com/sample/hello_world/ApplictionConfiguration.java
@@ -0,0 +1,11 @@
+package com.sample.hello_world;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ApplictionConfiguration {
+@RequestMapping("/hello")
+public String hello()
+{
+	return "Hello World";
+}
+}
---
+++ hello_world/src/main/java/com/sample/hello_world/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.sample.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
